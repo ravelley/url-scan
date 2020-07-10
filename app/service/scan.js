1612f2763355044e41510a8b4083c162cb85fc91
Ravelle Kelley <ravelle.kelley@gmail.com>
2020-07-10 15:24:07 -0400

    get ssl certificate

--- app/service/scan.js
+++ app/service/scan.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const path = require('path');
 const puppeteer = require('puppeteer-extra');
+const sslCertificate = require('get-ssl-certificate');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const db = require('../util/database');
 const logger = require('../util/logger');
@@ -10,27 +11,29 @@ const UrlMetaData = require('../model/url-metadata');
 puppeteer.use(StealthPlugin());
 
 const scanUrl = async (url, rescan) => {
+  const { href, hostname, protocol } = new URL(url);
+
   if (!rescan) {
-    const metadata = await metadataService.getMetadata(url);
+    const metadata = await metadataService.getMetadata(href);
     if (metadata) return metadata;
   } else {
-    // TODO: clean up screenshot
+    // TODO: remove old screenshot
     const sql = 'delete from url_metadata where url = ?';
-    const params = [url];
+    const params = [href];
     await db.call(sql, params);
   }
 
   const browser = await puppeteer.launch({ headless: true });
   try {
     const page = await browser.newPage();
-    const metadata = new UrlMetaData(url);
+    const metadata = new UrlMetaData(href);
 
     page.on('response', async (response) => {
       metadata.ipAddr = response.remoteAddress().ip;
     });
 
     await page.setViewport({ width: 800, height: 600 });
-    await page.goto(url);
+    await page.goto(href);
     // TODO: figure out better way to determine page load
     await page.waitFor(3000);
 
@@ -40,10 +43,12 @@ const scanUrl = async (url, rescan) => {
     metadata.screenshot = `image/${Date.now()}.png`;
     await page.screenshot({ path: `${path.resolve(__dirname)}/../../data/image/${fileName}`, fullPage: true });
 
+    if (protocol === 'https:') metadata.sslCertificate = await sslCertificate.get(hostname);
+
     metadata.timestamp = Date.now();
 
     const sql = 'insert into url_metadata (url, metadata) values (?, ?)';
-    const params = [url, JSON.stringify(metadata)];
+    const params = [href, JSON.stringify(metadata)];
     await db.call(sql, params);
 
     return metadata;
